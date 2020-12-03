93f812774c7ef3d360cc1028f1e60eb0064add0f
SamuelCurrid <samuelcurrid@gmail.com>
2020-12-03 00:22:58 -0500

    Day 3 solution

--- Day03/Solution.py
+++ Day03/Solution.py
@@ -1,15 +1,33 @@
 with open("input.txt") as data:
-    expenses = [int(i) for i in data.read().splitlines()]
+    rows = [i for i in data.read().splitlines()]
 
 
 # Part 1
 def part_1():
-    return
+    trees = 0
+
+    for i in range(0, len(rows)):
+        if rows[i][i * 3 % len(rows[i])] == "#":
+            trees += 1
+
+    print(f"There are {trees} trees")
 
 
 # Part 2
 def part_2():
-    return
+    trees = [0, 0, 0, 0, 0]
+
+    for i in range(0, len(rows)):
+
+        for j in range(0, 4):
+            if rows[i][i * (1 + (j * 2)) % len(rows[i])] == "#":
+                trees[j] += 1
+
+        if i % 2 == 0:
+            if rows[i][int(i * 0.5) % len(rows[i])] == "#":
+                trees[4] += 1
+
+    print(f"There are {trees[0] * trees[1] * trees[2] * trees[3] * trees[4]} trees")
 
 
 # Run solutions
