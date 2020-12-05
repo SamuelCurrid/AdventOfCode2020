d97eb4f5e41cd05a184df0ef521e67276ef96a60
SamuelCurrid <samuelcurrid@gmail.com>
2020-12-05 00:28:31 -0500

    Day 5 solution

--- Day05/Solution.py
+++ Day05/Solution.py
@@ -1,15 +1,37 @@
 with open("input.txt") as data:
-    expenses = [int(i) for i in data.read().splitlines()]
+    boardingPasses = [i for i in data.read().splitlines()]
 
 
 # Part 1
 def part_1():
-    return
+    highestID = 0
+
+    for boardingPass in boardingPasses:
+        row = int(boardingPass[:7].replace("F", "0").replace("B", "1"), 2)
+        col = int(boardingPass[-3:].replace("L", "0").replace("R", "1"), 2)
+
+        identification = (row * 8) + col
+
+        if identification > highestID:
+            highestID = identification
+
+    print(f"{highestID} is the highest seat ID")
 
 
 # Part 2
 def part_2():
-    return
+    seatsByRow = []
+    for boardingPass in boardingPasses:
+        row = int(boardingPass[:7].replace("F", "0").replace("B", "1"), 2)
+        col = int(boardingPass[-3:].replace("L", "0").replace("R", "1"), 2)
+
+        identification = (row * 8) + col
+        seatsByRow.append(identification)
+
+    seatsByRow.sort()
+    for i in range(0, len(seatsByRow) - 1):
+        if seatsByRow[i] + 1 != seatsByRow[i + 1]:
+            print(f"{seatsByRow[i] + 1} is your seat ID")
 
 
 # Run solutions
